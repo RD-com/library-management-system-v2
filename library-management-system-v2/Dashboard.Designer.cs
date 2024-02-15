1774ee8dfcd7bcb38058feaec2659c12aeb6112c
Kavindu Sanjula <ravanadevs@gmail.com>
2024-02-15 14:51:59 +0530

    Add project files.

---
+++ library-management-system-v2/Dashboard.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace library_management_system_v2
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnMember = new System.Windows.Forms.Button();
+            this.btnBorrowing = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnLoan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(136, 166);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(163, 56);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(305, 166);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(163, 56);
+            this.btnBook.TabIndex = 1;
+            this.btnBook.Text = "Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            // 
+            // btnMember
+            // 
+            this.btnMember.Location = new System.Drawing.Point(474, 166);
+            this.btnMember.Name = "btnMember";
+            this.btnMember.Size = new System.Drawing.Size(163, 56);
+            this.btnMember.TabIndex = 2;
+            this.btnMember.Text = "Member";
+            this.btnMember.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrowing
+            // 
+            this.btnBorrowing.Location = new System.Drawing.Point(474, 228);
+            this.btnBorrowing.Name = "btnBorrowing";
+            this.btnBorrowing.Size = new System.Drawing.Size(163, 56);
+            this.btnBorrowing.TabIndex = 5;
+            this.btnBorrowing.Text = "Borrowing";
+            this.btnBorrowing.UseVisualStyleBackColor = true;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(305, 228);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(163, 56);
+            this.btnReturn.TabIndex = 4;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnLoan
+            // 
+            this.btnLoan.Location = new System.Drawing.Point(136, 228);
+            this.btnLoan.Name = "btnLoan";
+            this.btnLoan.Size = new System.Drawing.Size(163, 56);
+            this.btnLoan.TabIndex = 3;
+            this.btnLoan.Text = "Loan";
+            this.btnLoan.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Library Management System";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBorrowing);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnLoan);
+            this.Controls.Add(this.btnMember);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.btnHome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnMember;
+        private System.Windows.Forms.Button btnBorrowing;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnLoan;
+        private System.Windows.Forms.Label label1;
+    }
+}
