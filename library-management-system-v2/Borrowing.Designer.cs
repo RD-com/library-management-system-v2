937800570f3fdcd6895ddbf6388b66e03ea499e6
Kavindu Sanjula <ravanadevs@gmail.com>
2024-02-16 13:38:34 +0530

    borrowing done

--- library-management-system-v2/Borrowing.Designer.cs
+++ library-management-system-v2/Borrowing.Designer.cs
@@ -31,6 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dgvBorrowings = new System.Windows.Forms.DataGridView();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Book = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Member = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IssuedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ReturnedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Returned = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBorrowings)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,10 +54,19 @@
             // dgvBorrowings
             // 
             this.dgvBorrowings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBorrowings.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Book,
+            this.Member,
+            this.UserID,
+            this.IssuedDate,
+            this.ReturnedDate,
+            this.Returned});
             this.dgvBorrowings.Location = new System.Drawing.Point(12, 52);
             this.dgvBorrowings.Name = "dgvBorrowings";
             this.dgvBorrowings.Size = new System.Drawing.Size(921, 461);
             this.dgvBorrowings.TabIndex = 1;
+            this.dgvBorrowings.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBorrowings_CellClick);
             // 
             // btnDelete
             // 
@@ -62,6 +78,42 @@
             this.btnDelete.TabIndex = 2;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Book
+            // 
+            this.Book.HeaderText = "Book";
+            this.Book.Name = "Book";
+            // 
+            // Member
+            // 
+            this.Member.HeaderText = "Member";
+            this.Member.Name = "Member";
+            // 
+            // UserID
+            // 
+            this.UserID.HeaderText = "UserID";
+            this.UserID.Name = "UserID";
+            // 
+            // IssuedDate
+            // 
+            this.IssuedDate.HeaderText = "IssuedDate";
+            this.IssuedDate.Name = "IssuedDate";
+            // 
+            // ReturnedDate
+            // 
+            this.ReturnedDate.HeaderText = "ReturnedDate";
+            this.ReturnedDate.Name = "ReturnedDate";
+            // 
+            // Returned
+            // 
+            this.Returned.HeaderText = "Returned";
+            this.Returned.Name = "Returned";
             // 
             // Borrowing
             // 
@@ -86,5 +138,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvBorrowings;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Book;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Member;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IssuedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ReturnedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Returned;
     }
 }
--- library-management-system-v2/Borrowing.cs
+++ library-management-system-v2/Borrowing.cs
@@ -12,9 +12,66 @@ namespace library_management_system_v2
 {
     public partial class Borrowing : Form
     {
+        private models.Borrowing _borrowing = null;
+
         public Borrowing()
         {
             InitializeComponent();
+            LoadData(); 
+        }
+
+        void LoadData()
+        {
+            var borrowings = models.Borrowing.GetAll();
+            foreach (var borrowing in borrowings)
+            {
+                var member = models.Member.GetOneWithUserID(borrowing.UserID);
+                var book = models.Book.GetOne(borrowing.BookID);
+
+                var row = new object[]
+                {
+                    borrowing.Id,
+                    book.Title,
+                    member.Name,
+                    member.UserID,
+                    borrowing.IssuedDate,
+                    borrowing.ReturnedDate,
+                    borrowing.Returned ? "Returned" : "Pending"
+                };
+
+                dgvBorrowings.Rows.Add(row);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(_borrowing == null)
+            {
+                MessageBox.Show("Plese select a record");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Are you sure you want to delete this record?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes)
+            {
+                int arc = _borrowing.Delete();
+                if(arc > 0)
+                {
+                    MessageBox.Show("Record deleted");
+                    LoadData();
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+
+        private void dgvBorrowings_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var id = Convert.ToInt32(dgvBorrowings.Rows[e.RowIndex].Cells[0].Value);
+            _borrowing = models.Borrowing.GetOne(id);
         }
     }
 }
