54a54e314c3e53e678bd81302aeb8277929d2435
Kavindu Sanjula <ravanadevs@gmail.com>
2024-02-16 13:56:38 +0530

    style change

--- library-management-system-v2/Dashboard.Designer.cs
+++ library-management-system-v2/Dashboard.Designer.cs
@@ -35,77 +35,109 @@
             this.btnReturn = new System.Windows.Forms.Button();
             this.btnLoan = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(136, 166);
+            this.btnHome.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.Location = new System.Drawing.Point(18, 57);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(163, 56);
             this.btnHome.TabIndex = 0;
             this.btnHome.Text = "Home";
-            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.UseVisualStyleBackColor = false;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
             // btnBook
             // 
-            this.btnBook.Location = new System.Drawing.Point(305, 166);
+            this.btnBook.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnBook.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBook.Location = new System.Drawing.Point(18, 119);
             this.btnBook.Name = "btnBook";
             this.btnBook.Size = new System.Drawing.Size(163, 56);
             this.btnBook.TabIndex = 1;
             this.btnBook.Text = "Book";
-            this.btnBook.UseVisualStyleBackColor = true;
+            this.btnBook.UseVisualStyleBackColor = false;
             this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
             // 
             // btnMember
             // 
-            this.btnMember.Location = new System.Drawing.Point(474, 166);
+            this.btnMember.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnMember.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMember.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMember.Location = new System.Drawing.Point(18, 180);
             this.btnMember.Name = "btnMember";
             this.btnMember.Size = new System.Drawing.Size(163, 56);
             this.btnMember.TabIndex = 2;
             this.btnMember.Text = "Member";
-            this.btnMember.UseVisualStyleBackColor = true;
+            this.btnMember.UseVisualStyleBackColor = false;
             this.btnMember.Click += new System.EventHandler(this.btnMember_Click);
             // 
             // btnBorrowing
             // 
-            this.btnBorrowing.Location = new System.Drawing.Point(474, 228);
+            this.btnBorrowing.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnBorrowing.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBorrowing.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrowing.Location = new System.Drawing.Point(18, 366);
             this.btnBorrowing.Name = "btnBorrowing";
             this.btnBorrowing.Size = new System.Drawing.Size(163, 56);
             this.btnBorrowing.TabIndex = 5;
             this.btnBorrowing.Text = "Borrowing";
-            this.btnBorrowing.UseVisualStyleBackColor = true;
+            this.btnBorrowing.UseVisualStyleBackColor = false;
             this.btnBorrowing.Click += new System.EventHandler(this.btnBorrowing_Click);
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(305, 228);
+            this.btnReturn.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.Location = new System.Drawing.Point(18, 304);
             this.btnReturn.Name = "btnReturn";
             this.btnReturn.Size = new System.Drawing.Size(163, 56);
             this.btnReturn.TabIndex = 4;
             this.btnReturn.Text = "Return";
-            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.UseVisualStyleBackColor = false;
             this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // btnLoan
             // 
-            this.btnLoan.Location = new System.Drawing.Point(136, 228);
+            this.btnLoan.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnLoan.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoan.Location = new System.Drawing.Point(18, 242);
             this.btnLoan.Name = "btnLoan";
             this.btnLoan.Size = new System.Drawing.Size(163, 56);
             this.btnLoan.TabIndex = 3;
             this.btnLoan.Text = "Loan";
-            this.btnLoan.UseVisualStyleBackColor = true;
+            this.btnLoan.UseVisualStyleBackColor = false;
             this.btnLoan.Click += new System.EventHandler(this.btnLoan_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(201, 87);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(384, 31);
+            this.label1.Size = new System.Drawing.Size(210, 31);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Library Management System";
+            this.label1.Text = "ABC Bookshop";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pictureBox1.Image = global::library_management_system_v2.Properties.Resources.shop;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(807, 456);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Dashboard
             // 
@@ -120,9 +152,11 @@
             this.Controls.Add(this.btnMember);
             this.Controls.Add(this.btnBook);
             this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Dashboard";
             this.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,5 +171,6 @@
         private System.Windows.Forms.Button btnReturn;
         private System.Windows.Forms.Button btnLoan;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
